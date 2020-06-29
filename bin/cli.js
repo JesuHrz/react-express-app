7f0b47599e63558b42d9bf8f980ae07020acca19
Jesus Hernandez <jesusbeckan@gmail.com>
2020-06-28 21:08:07 -0500

    Adds validation for invalid project names

--- bin/cli.js
+++ bin/cli.js
@@ -2,20 +2,30 @@
 
 const minimist = require('minimist')
 const inquirer = require('inquirer')
-const figlet = require('figlet')
 const chalk = require('chalk')
 
-const { message } = require('../lib')
+const { message, checkPackageName } = require('../lib')
 
 const argv = minimist(process.argv.slice(2))
 
 async function init () {
+  console.log()
   const msg = await message('React Express App')
   console.log(chalk.bold.cyan(msg))
+  console.log()
 
-  const command = argv._.shift()
+  const packageName = argv._.shift()
 
-  console.log(command)
+  if (packageName === undefined) {
+    console.log(chalk.red(
+      'You need to add a name to your project \n' +
+      'Example: react-express-app <project-name>'
+    ))
+    console.log()
+    process.exit(1)
+  }
+
+  checkPackageName(packageName)
 }
 
 init().catch(err => {
