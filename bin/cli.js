f0d32e608897327b145e3ddeba43dddd99c185c3
Jesus Hernandez <jesusbeckan@gmail.com>
2020-06-29 10:59:37 -0500

    Adds the questions that will be used in the generator

--- bin/cli.js
+++ bin/cli.js
@@ -14,16 +14,39 @@ async function init () {
     const msg = await message('React Express App')
     console.log(chalk.bold.cyan(msg))
   } catch (e) {
-    console.error(chalk.red(e))
+    console.error(chalk.red(e.stack))
     process.exit(1)
   }
 
   const packageName = argv._.shift()
 
   checkPackageName(packageName)
+
+  inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'redux',
+      message: 'Would you like to incorporate redux in your project?',
+      default: true
+    },
+    {
+      type: 'list',
+      name: 'preprocessor',
+      message: 'Which css preprocessor would you like to incorporate?',
+      choices: ['None', 'Sass', 'Less'],
+      default: 'None'
+    }
+  ])
+    .then(answers => {
+      console.log('answers', answers)
+    })
+    .catch(e => {
+      console.error(chalk.red(e.stack))
+      process.exit(1)
+    })
 }
 
-init().catch(err => {
-  console.error(err)
+init().catch(e => {
+  console.error(chalk.red(e.stack))
   process.exit(1)
 })
