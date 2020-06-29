add3077301f5f49ffe45f5df6929b218a31fe417
Jesus Hernandez <jesusbeckan@gmail.com>
2020-06-28 19:17:37 -0500

    Creates cli file

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,24 @@
 'use strict'
 
-console.warn('Hello world')
+const minimist = require('minimist')
+const inquirer = require('inquirer')
+const figlet = require('figlet')
+const chalk = require('chalk')
+
+const { message } = require('../lib')
+
+const argv = minimist(process.argv.slice(2))
+
+async function init () {
+  const msg = await message('React Express App')
+  console.log(chalk.bold.cyan(msg))
+
+  const command = argv._.shift()
+
+  console.log(command)
+}
+
+init().catch(err => {
+  console.log(err)
+  process.exit(1)
+})
