0e4658b566cc687c4f63536182d3ab13b7694e29
Jesus Hernandez <jesusbeckan@gmail.com>
2020-07-01 21:25:27 -0500

    Shows warning when the cli is running in development mode

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const chalk = require('chalk')
 const nodeVersion = process.versions.node
 const major = nodeVersion.split('.')[0]
 
+if (process.env.NODE_ENV === 'development') {
+  console.log()
+  console.log(chalk.yellow(
+    'You are running React Express App ' +
+    'in development mode.'
+  ))
+}
+
 if (major < 10) {
   console.log(chalk.red(
     `Your node version is ${major}, ` +
-    'in order to use react you need to have a node version equal or higher than 10'
+    'in order to use react you need to have a node version equal or higher than 10.'
   ))
   process.exit(1)
 }
