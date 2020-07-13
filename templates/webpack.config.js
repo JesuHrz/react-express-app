b06bfc5207e489bbf053ff2afa6a9ebe192388dd
Jesus Hernandez <jesusbeckan@gmail.com>
2020-07-12 21:21:17 -0500

    Optimizes and improves webpack

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -1,15 +1,32 @@
+'use strict'
+
 const path = require('path')
 const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = env => {
-  const plugins = [ new MiniCssExtractPlugin('css/style.css'),
+  const minimizer = []
+  const plugins = [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV)
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css'
     })
   ]
 
+  if (env.NODE_ENV === 'production') {
+    plugins.push(new CleanWebpackPlugin())
+    minimizer.push(new TerserJSPlugin({}))
+    minimizer.push(new OptimizeCSSAssetsPlugin({}))
+  }
+
   return {
+    mode: env.NODE_ENV,
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
       path: path.resolve(__dirname, 'dist/'),
@@ -18,8 +35,17 @@ module.exports = env => {
     devServer: {
       publicPath: '/dist',
       host: '0.0.0.0',
-      port: 9000,
+      port: 8080,
       historyApiFallback: true
+    },
+    resolve: {
+      alias: {
+        pages: path.resolve(__dirname, 'src/pages'),
+        components: path.resolve(__dirname, 'src/components'),
+        actions: path.resolve(__dirname, 'src/actions'),
+        lib: path.resolve(__dirname, 'src/lib'),
+        assets: path.resolve(__dirname, 'assets')
+      }
     },
     module: {
       rules: [
@@ -34,10 +60,10 @@ module.exports = env => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env', '@babel/preset-react'],
-              'plugins': [
+              plugins: [
                 '@babel/plugin-syntax-dynamic-import',
                 '@babel/plugin-syntax-import-meta',
-                ['@babel/plugin-proposal-class-properties', { 'loose': false }],
+                ['@babel/plugin-proposal-class-properties', { loose: false }],
                 '@babel/plugin-proposal-json-strings',
                 '@babel/plugin-transform-runtime'
               ]
@@ -64,6 +90,9 @@ module.exports = env => {
         }
       ]
     },
-    plugins
+    optimization: {
+      minimizer
+    },
+    plugins: [...plugins, new HtmlWebpackPlugin({ template: './index.html' })]
   }
 }
